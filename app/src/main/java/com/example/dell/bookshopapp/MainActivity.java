dd9e2683dc561246b3ad8decdaf785b8c7223413
agabairweritah <agabairweritah@gmail.com>
2019-04-10 15:39:18 +0300

    This has Intents both implicit and explicit

--- app/src/main/java/com/example/dell/bookshopapp/MainActivity.java
+++ app/src/main/java/com/example/dell/bookshopapp/MainActivity.java
@@ -1,15 +1,20 @@
 package com.example.dell.bookshopapp;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn;
 
     //AGABAIRWE RITAH
 
@@ -19,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn=(Button) findViewById(R.id.button);
     }
     void setfragment(Fragment fragment){
         getSupportFragmentManager().beginTransaction().replace(R.id.container,fragment).commit();
@@ -62,6 +68,7 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater inflater=getMenuInflater();
         getMenuInflater().inflate(R.menu.item,menu);
         return super.onCreateOptionsMenu(menu);
+
     }
 
     @Override
@@ -84,5 +91,16 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+
+        public void show(View view) {
+            Toast toasttop = Toast.makeText(MainActivity.this, "Welcome to Arsitoc Book shop", Toast.LENGTH_SHORT);
+            toasttop.setGravity(Gravity.TOP,0,0);
+            toasttop.show();
+            Intent implicitIntent = new Intent(Intent.ACTION_VIEW);
+            implicitIntent.setData(Uri.parse("http://aristocbooklexltd.tradenote.net/"));
+            startActivity(implicitIntent);
+        }
+
 
 }
